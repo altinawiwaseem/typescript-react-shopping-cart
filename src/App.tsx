df99d496cfb33500fe630990f2fe68ae974084a2
altinawiwaseem <wasemm@live.com>
2023-06-04 17:30:34 +0200

    create the pages

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Store from "./pages/Store";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/store" element={<Store />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </Container>
+    </>
+  );
+}
+
+export default App;
