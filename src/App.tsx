2e2675e9b972699ef6b859ff0028ba215a076910
altinawiwaseem <wasemm@live.com>
2023-06-04 18:24:04 +0200

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Container } from "react-bootstrap";
 import Store from "./pages/Store";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Container>
+      <Navbar />
+      <Container className="mb-4">
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/store" element={<Store />}></Route>
